1f64be66a96eb3f1a8f2ff49fb9727fa2044df46
rehadhasan <rehadhasan664@gmail.com>
2024-11-22 02:33:05 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+// const port = 3000;
+
+// parsers
+app.use(express.json());
+app.use(cors());
+
+// application route
+// app.use('/api/v1/students', StudentRoutes);
+
+app.get('/', (req: Request, res: Response) => {});
+
+export default app;
